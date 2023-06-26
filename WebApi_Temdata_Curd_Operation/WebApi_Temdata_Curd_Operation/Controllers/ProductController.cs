bb1c97c275aafb50f5795ab056d57814f0317001
MD. NIZAM UDDIN TUHIN <112401451+nizambhl2001@users.noreply.github.com>
2023-06-26 10:09:04 +0600

    Add files via upload

---
+++ WebApi_Temdata_Curd_Operation/WebApi_Temdata_Curd_Operation/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_Temdata_Curd_Operation.Models;
+
+namespace WebApi_Temdata_Curd_Operation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> ListProduct = new List<Product>() {
+         new Product() { Id = 1,ProductName="Nokia N77",Descripsion="Good Product"} ,
+         new Product() { Id = 2,ProductName="HP 840 G2",Descripsion="Good Product"},
+         new Product() { Id = 3,ProductName="Sony Ranks",Descripsion="Good Product"}
+        };
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ListProduct;
+        }
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+            ListProduct.Add(product);
+        } 
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody] Product product)
+        {
+            ListProduct[id] = product;
+        } 
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ListProduct.RemoveAt(id);
+        }
+
+    }
+}
---
+++ WebApi_Temdata_Curd_Operation/WebApi_Temdata_Curd_Operation/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_Temdata_Curd_Operation.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        
+        public string Descripsion { get; set; }
+    }
+}
